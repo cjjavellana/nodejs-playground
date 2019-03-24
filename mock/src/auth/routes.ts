4b1d616ed2fb3afef704e43bd9cd5d9f31fc839d
Christian Javellana <cjjavellana@gmail.com>
2019-03-24 09:04:32 +0800

    Dockerize mock service

--- mock/src/auth/routes.ts
+++ mock/src/auth/routes.ts
@@ -1,8 +1,36 @@
 import { Application, NextFunction, Request, Response } from "express";
 
-interface LoginRequest {
-    username: string;
-    password: string;
+class LoginRequest {
+    public username: string;
+    public password: string;
+}
+
+enum Authority {
+    ALL, READ, MODIFY, DELETE
+}
+
+class Permission {
+    public module: string;
+    public name: string;
+    public authority: Authority;
+
+    constructor(module: string, name: string, authority: Authority) {
+        this.module = module;
+        this.name = name;
+        this.authority = authority;
+    }
+}
+
+class UserAuthority {
+
+    public username: string;
+    public permissions: Permission[];
+
+    constructor(username: string, permissions: Permission[]) {
+        this.username = username;
+        this.permissions = permissions;
+    }
+
 }
 
 /**
@@ -11,10 +39,28 @@ interface LoginRequest {
  */
 export const register = (app: Application) => {
 
+    /**
+     * Performs a mock authentication.
+     *
+     * Accounts:
+     *
+     */
     app.post("/auth/api/v1/login", (req: Request, res: Response, _: NextFunction) => {
         const loginReq = req.body as LoginRequest;
         console.log("Username: %s; Password: %s", loginReq.username, loginReq.password);
-        res.send("");
+        if (loginReq.username === "aaa") {
+            res.header("X-Auth-Token", "aaabbbcccdddeeefff");
+            res.send(roleSearch());
+        } else {
+            res.status(401).send("Not authorized");
+        }
     });
 
+};
+
+const roleSearch = () => {
+    return new UserAuthority("aaa", [
+        new Permission("equities-healthcare", "search", Authority.READ),
+        new Permission("equities-aviation", "search", Authority.ALL),
+    ]);
 };
