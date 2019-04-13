f38fcfabe8fc646761fe2baa129780fa811ce906
Christian Javellana <cjjavellana@gmail.com>
2019-04-13 08:44:00 +0800

    Make ELK logging through RabbitMQ Work

--- src/logger.ts
+++ src/logger.ts
@@ -4,25 +4,40 @@ import log4js, { LoggingEvent } from "log4js";
 export const register = (app: Application) => {
     log4js.addLayout("json", (config: any) => {
         return (logEvent: LoggingEvent) => {
-            return JSON.stringify(logEvent) + config.separator;
+            return JSON.stringify(logEvent);
         };
     });
 
     log4js.configure({
         appenders: {
-            out: { type: "stdout", layout: { type: "json", separator: "," } }
+            console: { type: "console" },
+            /* tslint:disable:object-literal-sort-keys */
+            logstash: {
+                type: "@log4js-node/rabbitmq",
+                host: process.env.RABBITMQ_HOST,
+                port: process.env.RABBITMQ_PORT,
+                username: process.env.RABBITMQ_USER,
+                password: process.env.RABBITMQ_PASSWORD,
+                exchange: "logs",
+                mq_type: "direct",
+                routing_key: "logstash",
+                durable: true,
+                layout: { type: "json", separator: "," }
+            }
+            /* tslint:enable:object-literal-sort-keys */
         },
         categories: {
-            default: { appenders: ["out"], level: "info" }
+            default: { appenders: ["console"], level: "info" },
+            logstash: { appenders: ["logstash", "console"], level: "info" }
         }
     });
 
-    const logger = log4js.getLogger("default");
+    const logger = log4js.getLogger("logstash");
     app.locals.logger = logger;
     app.use(log4js.connectLogger(logger, {
         format: (req, res, format) => {
             const reqId = req.headers["x-request-id"] || "";
-            return format(":remote-addr - -" + reqId + " \":method :url HTTP/:http-version\"" +
+            return format(":remote-addr - - " + reqId + " \":method :url HTTP/:http-version\"" +
                 " :response-time :status :content-length");
         },
         level: "auto",
