57957b35d3498084456e50abb35d3665c2fe426e
Christian Javellana <cjjavellana@gmail.com>
2019-04-16 07:57:06 +0800

    Refactoring

--- src/loadenv.ts
+++ src/loadenv.ts
@@ -2,22 +2,55 @@ import dotenv from "dotenv";
 
 export const config = () => {
     dotenv.config();
+    Configurations.ConfigResolver.resolve(process.env);
+};
 
-    // multiline env fix
-    for (const key of Object.keys(process.env)) {
-        try {
-            const val = process.env[key];
-            if (hasNewLine(val)) {
-                process.env[key] = JSON.parse(`"${process.env[key]}"`);
-            }
-        } catch (error) {
-            /* tslint:disable:no-console */
-            console.log("Unable to convert %s", error);
-            /* tslint:enable:no-console */
+export namespace Configurations {
+
+    export class ConfigResolver {
+
+        private env: NodeJS.ProcessEnv;
+
+        private constructor(env: NodeJS.ProcessEnv) {
+            this.env = env;
         }
-    }
-};
 
-const hasNewLine = (val: string) => {
-    return val.indexOf("\\n") !== -1;
-};
+        protected init() {
+            for (const key of Object.keys(process.env)) {
+                try {
+                    const val = process.env[key];
+                    if (this.hasNewLine(val)) {
+                        // Only resolve entries having \n in them
+                        process.env[key] = this.fixMultilineValue(val);
+                    }
+                } catch (error) {
+                    // env resolution happens before logging system initialization
+
+                    /* tslint:disable:no-console */
+                    console.log("Unable to convert %s", error);
+                    /* tslint:enable:no-console */
+                }
+            }
+        }
+
+        protected fixMultilineValue(val: string) {
+            return JSON.parse(`"${val}"`);
+        }
+
+        protected hasNewLine(val: string) {
+            return val.indexOf("\\n") !== -1;
+        }
+
+        protected isEncrypted(val: string) {
+            return val.startsWith("[enc]");
+        }
+
+        public static resolve(env: NodeJS.ProcessEnv): ConfigResolver {
+            const envConfig = new ConfigResolver(env);
+            envConfig.init();
+            return envConfig;
+        }
+
+    }
+}
+
